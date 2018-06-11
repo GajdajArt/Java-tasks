63bf1a171ef6c2986e8e305384240c0c691f578b
gajdaj <artem.gajdaj@nixsolutions.com>
2018-06-11 16:13:44 +0300

    update Chapter3Task4

--- src/Chapter3Task4.java
+++ src/Chapter3Task4.java
@@ -17,19 +17,31 @@ public class Chapter3Task4 {
 
         for (int i = 0; i < sizeOfArray; i++) {
             for (int j = 0; j < sizeOfArray; j++) {
-                for (int k = 0; k < sizeOfArray; k++) {
-                    for (int m = 0; m < sizeOfArray; m++) {
-                        for (int n = 0; n < sizeOfArray; n++) {
-                            for (int o = 0; o < sizeOfArray; o++) {
-                                itemOfArray++;
-                                multidimensionalArray[i][j][k][m][n][o] = itemOfArray;
-                            }
-                        }
-                    }
-                }
+                getFirstAttachments(i, j, multidimensionalArray, sizeOfArray, itemOfArray);
             }
         }
 
         return multidimensionalArray;
+    }
+
+    private static void getFirstAttachments(int i, int j, int[][][][][][] multidimensionalArray,
+            int sizeOfArray, int itemOfArray){
+
+        for (int k = 0; k < sizeOfArray; k++) {
+            for (int m = 0; m < sizeOfArray; m++) {
+                getSecondAttachments(i, j, k, m, multidimensionalArray, sizeOfArray, itemOfArray);
+            }
+        }
+    }
+
+    private static void getSecondAttachments(int i, int j, int k, int m,
+            int[][][][][][] multidimensionalArray, int sizeOfArray, int itemOfArray) {
+
+        for (int n = 0; n < sizeOfArray; n++) {
+            for (int o = 0; o < sizeOfArray; o++) {
+                itemOfArray++;
+                multidimensionalArray[i][j][k][m][n][o] = itemOfArray;
+            }
+        }
     }
 }
