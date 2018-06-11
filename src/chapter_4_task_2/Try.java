fb106709d44430866c1057c9c6ec7963624914b0
GajdajArt <Gajdajart@gmail.com>
2018-06-11 21:44:45 +0300

    update chapter_4_task_2

---
+++ src/chapter_4_task_2/Try.java
@@ -0,0 +1,16 @@
+package chapter_4_task_2;
+
+public class Try {
+
+    private static final String ITEM = "item ";
+
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+
+        for (int i = 0; i < 120; i++) {
+            vector.add(ITEM + i);
+        }
+
+        vector.showAllItems();
+    }
+}
