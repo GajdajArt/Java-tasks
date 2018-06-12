5c644c3bb95abc69fcf2e5d3915ad5dbb4e14916
gajdaj <artem.gajdaj@nixsolutions.com>
2018-06-12 17:47:28 +0300

    chapter 5

---
+++ src/chapter_5_task_1/Point.java
@@ -0,0 +1,18 @@
+package chapter_5_task_1;
+
+public abstract class Point {
+
+    private int x;
+    private int y;
+
+    abstract void print();
+
+    abstract void scale(int scaleCoefficient);
+
+    public void move(int x, int y) {
+
+        this.x += x;
+        this.y += y;
+
+    }
+}
---
+++ src/chapter_5_task_1/Thriangle.java
@@ -0,0 +1,14 @@
+package chapter_5_task_1;
+
+public class Thriangle extends Point{
+
+    @Override
+    void print() {
+
+    }
+
+    @Override
+    void scale(int scaleCoefficient) {
+
+    }
+}
