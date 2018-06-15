375103e7f28459b7189a2403c08686ed2c24ed25
gajdaj <artem.gajdaj@nixsolutions.com>
2018-06-15 19:03:16 +0300

    update chapter 10

--- src/Chapter10Task4.java
+++ src/Chapter10Task4.java
@@ -4,25 +4,12 @@ import java.util.List;
 
 public class Chapter10Task4 {
 
-    private static final String LINE = "Some   English Text  ds";
+    private static final String LINE = "Some   English   Text  ds";
 
     public static void main(String[] args) {
 
-        int size = 0;
-
-        List<String> myList = new ArrayList<String>(Arrays.asList(LINE.split(" ")));
+        String replaceSpace = LINE.replaceAll("\\s+", " ");
+        List<String> myList = new ArrayList<String>(Arrays.asList(replaceSpace.split(" ")));
         System.out.println(myList.size());
-
-            if (LINE.length() > 0) {
-                size = 1;
-
-                for (int i = 1; i < LINE.length(); i++) {
-                    if ((LINE.charAt(i) != ' ') && (LINE.charAt(i - 1) == ' ')) {
-                        size++;
-                    }
-            }
-        }
-
-        System.out.println(size);
     }
 }
